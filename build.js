94b1ad4162c3579d155175cfb0ad4b854a612415
aldia_mechdream <futami16237@gmail.com>
2026-01-23 21:02:17 +0800

    feat: 编写build脚本。改ts准备

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
-import fs from 'node:fs';
+import { execSync } from 'node:child_process';
 
 function main() {
   const frameworks = ['ktjs', 'vue'];
   for (const fw of frameworks) {
+    execSync(`vite build ${fw} --config ${fw}/vite.config.ts`);
   }
 }
+main();
